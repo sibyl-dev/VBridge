e186aab80c991f3f66a6a8a35186072b30a00a0d
ChengFR <459763742@qq.com>
2021-03-04 01:30:28 +0800

    Update Feature View

--- client/src/components/FeatureView/index.tsx
+++ client/src/components/FeatureView/index.tsx
@@ -6,22 +6,23 @@ import "./index.css"
 import { getFeatureValues, getPrediction, getSHAPValues } from "router/api";
 import { DataFrame, IDataFrame } from "data-forge";
 import * as _ from "lodash"
-import { getScaleLinear } from "visualization/common";
-import { ArrowDownOutlined, ArrowUpOutlined, SortAscendingOutlined } from "@ant-design/icons"
+import { getScaleLinear, beautifulPrinter } from "visualization/common";
+import { ArrowDownOutlined, ArrowUpOutlined, CaretRightOutlined, SortAscendingOutlined } from "@ant-design/icons"
+import { ScaleLinear } from "d3";
 
 const { Search } = Input;
 
 export interface FeatureViewProps {
     patientMeta?: PatientMeta,
-    featureMeta: DataFrame<number, FeatureMeta>,
+    featureMeta: IDataFrame<number, FeatureMeta>,
     predictionTargets: string[]
 }
 
 export interface FeatureViewStates {
-    predictions?: (target: string) => number
-    featureDisplayValues?: DataFrame
-    features?: DataFrame<number, any> |
-    IDataFrame<number, any>,
+    target: string,
+    predictions?: (target: string) => number,
+    featureDisplayValues?: DataFrame,
+    features?: IDataFrame<number, Feature>,
 }
 
 export default class FeatureView extends React.Component<FeatureViewProps, FeatureViewStates> {
@@ -29,7 +30,11 @@ export default class FeatureView extends React.Component<FeatureViewProps, Featu
     constructor(props: FeatureViewProps) {
         super(props);
 
-        this.state = {};
+        this.state = {
+            target: this.props.predictionTargets[0]
+        };
+        this.defaultCellWidth = this.defaultCellWidth.bind(this);
+        this.onSelectTarget = this.onSelectTarget.bind(this);
     }
 
     private async updatePrediction() {
@@ -40,27 +45,51 @@ export default class FeatureView extends React.Component<FeatureViewProps, Featu
         }
     }
 
+    private defaultCellWidth(id: number) {
+        const width = [120, 100, 120];
+        return width[id];
+    }
+
+    private onSelectTarget(target: string) {
+        this.setState({ target });
+    }
+
     private async updateFeatures() {
         const { patientMeta, featureMeta, predictionTargets } = this.props
+        const { target } = this.state;
         const subject_id = patientMeta?.subjectId;
         if (subject_id !== undefined) {
             const featureValues = await getFeatureValues({ subject_id });
-            const shapValues = await getSHAPValues({ subject_id, target: 'complication' });
-            const features = featureMeta.select(row => {
-                const name = row['name'];
-                const value = featureValues(name);
+            const shapValues = await getSHAPValues({ subject_id, target });
+            const rawFeatures: IDataFrame<number, Feature> = featureMeta.select(row => {
                 return {
-                    Name: row['alias'],
-                    Value: (typeof (value) === typeof (0.0)) ? _.round(value, 3) : value,
-                    Contribution: _.round(shapValues(row['name']), 3)
-                }
-            })
+                    ...row,
+                    value: featureValues(row['name']),
+                    contribution: shapValues(row['name']),
+                };
+            });
+            const individualFeatures = rawFeatures.where(row => row.where_item.length == 0);
+            const whereFeatures = rawFeatures.where(row => row.where_item.length > 0);
+            const groups = whereFeatures.groupBy(row => row.where_item[1]).toArray();
+            const groupedFeature: IDataFrame<number, Feature> = new DataFrame(groups.map(group => {
+                const sample = group.first();
+                return {
+                    ...sample,
+                    alias: sample.where_item![1] as string,
+                    value: undefined,
+                    primitive: undefined,
+                    contribution: _.sum(group.getSeries('contribution').toArray()),
+                    children: group
+                };
+            }));
+            const features = individualFeatures.concat(groupedFeature);
             this.setState({ features })
         }
     }
 
-    componentDidUpdate(prevProps: FeatureViewProps) {
-        if (prevProps.patientMeta?.subjectId !== this.props.patientMeta?.subjectId) {
+    componentDidUpdate(prevProps: FeatureViewProps, prevState: FeatureViewStates) {
+        if (prevProps.patientMeta?.subjectId !== this.props.patientMeta?.subjectId
+            || prevState.target !== this.state.target) {
             this.updatePrediction();
             this.updateFeatures();
         }
@@ -68,14 +97,18 @@ export default class FeatureView extends React.Component<FeatureViewProps, Featu
 
     public render() {
         const { predictionTargets } = this.props;
-        const { predictions, features } = this.state;
+        const { predictions, features, target } = this.state;
 
         return (
             <div style={{ height: "100%", width: "100%" }}>
-                {predictionTargets && ProbaList({ predictionTargets, predictions })}
+                {predictionTargets && ProbaList({
+                    predictionTargets, predictions,
+                    selected: target, onClick: this.onSelectTarget
+                })}
                 <Divider />
                 {features && <FeatureList
                     features={features}
+                    cellWidth={this.defaultCellWidth}
                 />}
             </div>
         )
@@ -84,27 +117,31 @@ export default class FeatureView extends React.Component<FeatureViewProps, Featu
 
 function ProbaList(params: {
     predictionTargets: string[],
-    predictions?: (target: string) => number
+    predictions?: (target: string) => number,
+    selected?: string,
+    onClick?: (value: string) => void,
 }) {
-    const { predictionTargets, predictions } = params
+    const { predictionTargets, predictions, selected, onClick } = params
     return <div className="proba-list">
         {predictionTargets.map(target =>
-            <Button block key={target} className="proba-item">
+            <Button block key={target} className={"proba-item" + (selected && target === selected ? " proba-selected" : "")}
+                onClick={() => onClick && onClick(target)}>
                 <div className="proba-target-name">{target}</div>
                 <div className="proba-value">{predictions ? predictions(target).toFixed(2) : '-'}</div>
-                <div className="proba-bar" style={{ height: "100%", width: `${predictions ? predictions(target) * 80 : 1}%` }}></div>
+                <div className="proba-bar" style={{ height: "100%", width: `${predictions ? predictions(target) * 100 : 1}%` }}></div>
             </Button>
         )}
     </div>
 }
 
 export interface FeatureListProps {
-    features: DataFrame<number, any> | IDataFrame<number, any>
+    features: IDataFrame<number, Feature>,
+    cellWidth: (id: number) => number,
 }
 
 export interface FeatureListStates {
     // Contribution sorting order
-    order?: 'ascending' | 'dscending'
+    order?: 'ascending' | 'dscending',
 }
 
 export class FeatureList extends React.Component<FeatureListProps, FeatureListStates> {
@@ -114,73 +151,160 @@ export class FeatureList extends React.Component<FeatureListProps, FeatureListSt
         this.state = {};
 
         this.onClick = this.onClick.bind(this);
+        this.getContributions = this.getContributions.bind(this);
+        this.sortFeatures = this.sortFeatures.bind(this);
     }
 
     private onClick(newOrder?: 'ascending' | 'dscending') {
-
         this.setState({ order: newOrder })
     }
 
-    protected featureBlock() {
+    private getContributions(features: IDataFrame<number, Feature>): number[] {
+        let contributions = features.getSeries('contribution').toArray();
+        for (const feature of features) {
+            if (feature.children) {
+                contributions = [...contributions, ...this.getContributions(feature.children)]
+            }
+        }
+        return contributions
+    }
 
+    private sortFeatures(features: IDataFrame<number, any>): IDataFrame<number, any> {
+        const { order } = this.state;
+        if (order === 'ascending')
+            features = features.orderBy(row => row.contribution);
+        else if (order == 'dscending')
+            features = features.orderBy(row => -row.contribution);
+        return features.select(feature => {
+            if (feature.children) {
+                return {
+                    ...feature,
+                    children: this.sortFeatures(feature.children)
+                }
+            }
+            else return feature
+        })
     }
 
     public render() {
+        const { features, cellWidth } = this.props;
         const { order } = this.state;
-        let features = this.props.features;
-        if (order === 'ascending')
-            features = features.orderBy(row => row.Contribution);
-        else if (order == 'dscending')
-            features = features.orderBy(row => -row.Contribution);
-        const width = 120;
-        const contributions = features?.getSeries('Contribution');
-        const x = contributions && getScaleLinear(0, width, contributions);
-
+        const sortedFeatures = this.sortFeatures(features);
+        const x = getScaleLinear(0, cellWidth(2), this.getContributions(sortedFeatures));
 
         return <div style={{ width: "100%" }}>
             <Search placeholder="input search text" style={{ marginLeft: 10, marginRight: 10, width: "90%" }} enterButton />
             <div style={{ width: "100%" }}>
                 <div className="feature-header">
-                    <div className="feature-header-text">
+                    <div style={{ width: 20 }} />
+                    <div className="feature-header-cell" style={{ width: cellWidth(0) }}>
                         <span>Name</span>
                         <SortAscendingOutlined />
                     </div>
-                    <div className="feature-header-text">Value</div>
-                    <div className="feature-header-text">
+                    <div className="feature-header-cell" style={{ width: cellWidth(1) }}>Value</div>
+                    <div className="feature-header-cell" style={{ width: cellWidth(2) }}>
                         <span>Contribution</span>
                         {order === 'dscending' ? <ArrowDownOutlined onClick={this.onClick.bind(this, 'ascending')} />
                             : <ArrowUpOutlined onClick={this.onClick.bind(this, 'dscending')} />}
                     </div>
                 </div>
-                {features?.toArray().map(row =>
-                    <div className="feature-block" key={row.Name}>
-                        <Tooltip title={row.Name}>
-                            <div className="feature-block-text feature-name">
-                                {row.Name.length > 12 ? row.Name.substring(0, 12) + "..." : row.Name}
-                            </div>
-                        </Tooltip>
-                        <Tooltip title={row.Value}>
-                            <div className="feature-block-text feature-value">
-                                {typeof (row.Value) === typeof ("") && row.Value.length > 12 ? row.Value.substring(0, 12) + "..." : row.Value}
-                            </div>
-                        </Tooltip>
-                        <div className={"feature-block-text feature-contribution"}>
-                            {row.Contribution > 0 ?
-                                <div className="pos-feature"
-                                    style={{
-                                        width: x!(row.Contribution) - x!(0),
-                                        marginLeft: x!(0)
-                                    }} /> :
-                                <div className="neg-feature"
-                                    style={{
-                                        width: x!(0) - x!(row.Contribution),
-                                        marginLeft: x!(row.Contribution)
-                                    }} />
-                            }
-                        </div>
-                    </div>
-                )}
+                <div className="feature-content">
+                    {sortedFeatures?.toArray().map(row =>
+                        <FeatureBlock
+                            feature={row}
+                            x={x!}
+                            cellWidth={cellWidth}
+                            key={row.name}
+                        />
+                    )}
+                </div>
             </div>
+        </div>
+    }
+}
+
+export interface FeatureBlockProps {
+    feature: Feature,
+    x: ScaleLinear<number, number>,
+    cellWidth: (id: number) => number,
+}
+export interface FeatureBlockStates {
+    collapsed: boolean,
+    expanded: boolean
+}
+
+export class FeatureBlock extends React.Component<FeatureBlockProps, FeatureBlockStates> {
+    constructor(props: FeatureBlockProps) {
+        super(props);
+        this.state = {
+            collapsed: true,
+            expanded: false
+        }
+
+        this.onClickButton = this.onClickButton.bind(this);
+        this.onClickDiv = this.onClickDiv.bind(this)
+    }
+
+    componentDidUpdate(prevProps: FeatureBlockProps) {
+        if (prevProps.feature.name !== this.props.feature.name) {
+            this.setState({ collapsed: true });
+        }
+    }
+
+    protected onClickButton() {
+        this.setState({ collapsed: !this.state.collapsed });
+    }
+
+    protected onClickDiv() {
+        this.setState({ expanded: !this.state.expanded });
+    }
+
+    render() {
+        const { feature, x, cellWidth } = this.props;
+        const { collapsed, expanded } = this.state
+        const { name, alias, value, contribution, children } = feature;
+        return <div>
+            <div style={{ display: "flex", justifyContent: "flex-end" }}>
+                <div style={{ width: 20 }}>
+                    {children && <CaretRightOutlined className="right-button"
+                        onClick={this.onClickButton} rotate={collapsed ? 0 : 90} />}
+                </div>
+                <div className="feature-block" key={name} style={{ height: expanded ? 100 : 30 }}
+                    onClick={children ? this.onClickButton : this.onClickDiv}>
+                    <Tooltip title={alias}>
+                        <div className="feature-block-cell feature-name" style={{ width: cellWidth(0) }}>
+                            {beautifulPrinter(alias)}
+                        </div>
+                    </Tooltip>
+                    <Tooltip title={value}>
+                        <div className={"feature-block-cell" + (children ? " feature-group-value" : " feature-value")}
+                            style={{ width: cellWidth(1) }}>
+                            {beautifulPrinter(value)}
+                        </div>
+                    </Tooltip>
+                    <div className={"feature-block-cell feature-contribution"} style={{ width: cellWidth(2) }}>
+                        {contribution > 0 ?
+                            <div className="pos-feature"
+                                style={{
+                                    width: x(contribution) - x(0),
+                                    marginLeft: x(0)
+                                }} /> :
+                            <div className="neg-feature"
+                                style={{
+                                    width: x(0) - x(contribution),
+                                    marginLeft: x(contribution)
+                                }} />
+                        }
+                    </div>
+                </div>
+            </div>
+            {!collapsed && children?.toArray().map(feature =>
+                <FeatureBlock
+                    feature={feature}
+                    x={x!}
+                    cellWidth={id => id === 0 ? cellWidth(id) - 10 : cellWidth(id)}
+                    key={feature.name}
+                />)}
         </div>
     }
 }
