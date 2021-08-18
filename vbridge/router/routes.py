c121b81d3dff5d582da7ad2084a090cc6d4f035f
ChengFR <459763742@qq.com>
2021-08-18 10:06:01 +0800

    Update routers

--- vbridge/router/routes.py
+++ vbridge/router/routes.py
@@ -18,16 +18,17 @@ def add_routes(app):
 
     Swagger(app, config=swagger_config, template=swagger_tpl, parse=True)
 
+    api.add_resource(res.task.Task, API + 'tasks/')
     # patient
     api.add_resource(res.patient.StaticInfo, API + 'patient_statics/<string:subject_id>')
     api.add_resource(res.patient.TemporalInfo, API + 'patient_temporal/<string:subject_id>')
     # feature
-    api.add_resource(res.feature.FeatureMeta, API + 'feature_schemas/')
+    api.add_resource(res.feature.FeatureSchema, API + 'feature_schemas/')
     api.add_resource(res.feature.FeatureMatrix, API + 'feature_values/')
     api.add_resource(res.feature.FeatureValues, API + 'feature_values/<string:subject_id>')
     # entity-set
-    api.add_resource(res.entity_set.EntitySetSchema, API + 'entity_schema/')
-    api.add_resource(res.entity_set.EntitySchema, API + 'entity_schema/<string:entity_id>')
+    api.add_resource(res.entityset.EntitySetSchema, API + 'entity_schema/')
+    api.add_resource(res.entityset.EntitySchema, API + 'entity_schema/<string:entity_id>')
     api.add_resource(res.entity_filter.ColumnExtents, API + 'column_extents/')
     api.add_resource(res.entity_filter.PatientSelection, API + 'patient_selection/')
     api.add_resource(res.reference_value.ReferenceValues, API + 'reference_values/')
@@ -36,7 +37,7 @@ def add_routes(app):
     # model
     api.add_resource(res.prediction.Prediction, API + 'predictions/<string:subject_id>')
     # explanation
-    api.add_resource(res.explanation.ShapValues, API + 'shap_values/<string:subject_id>')
-    api.add_resource(res.explanation.ShapValuesIfNormal, API +
+    api.add_resource(res.feature_explanation.ShapValues, API + 'shap_values/<string:subject_id>')
+    api.add_resource(res.feature_explanation.ShapValuesIfNormal, API +
                      'counterfactual_shap_values/<string:subject_id>')
     # api.add_resource(res.signal_explanation.SignalExplanation, API + 'signal_explanations/')
