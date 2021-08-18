2b1b9147fcf8aee9740c3cc619c072598d136825
ChengFR <459763742@qq.com>
2021-08-18 22:49:59 +0800

    Update numpy json encoder

--- vbridge/utils/router_helpers.py
+++ vbridge/utils/router_helpers.py
@@ -1,21 +1,39 @@
+import datetime
+
 import numpy as np
-from flask.json import JSONEncoder
+import json
 
 
 # From https://stackoverflow.com/questions/50916422/python-typeerror-object-of-type-int64-is-not
 # -json-serializable
-class NpEncoder(JSONEncoder):
+class NpEncoder(json.JSONEncoder):
+    """ Custom encoder for numpy data types """
+
     def default(self, obj):
-        if isinstance(obj, np.integer):
+        if isinstance(obj, (np.int_, np.intc, np.intp, np.int8,
+                            np.int16, np.int32, np.int64, np.uint8,
+                            np.uint16, np.uint32, np.uint64)):
+
             return int(obj)
-        elif isinstance(obj, np.floating):
+
+        elif isinstance(obj, (np.float_, np.float16, np.float32, np.float64)):
             return float(obj)
-        elif isinstance(obj, np.ndarray):
+
+        elif isinstance(obj, (np.complex_, np.complex64, np.complex128)):
+            return {'real': obj.real, 'imag': obj.imag}
+
+        elif isinstance(obj, (np.ndarray,)):
             return obj.tolist()
-        elif isinstance(obj, np.datetime64):
-            return str(obj)
-        else:
-            return super(NpEncoder, self).default(obj)
+
+        elif isinstance(obj, np.bool_):
+            return bool(obj)
+
+        elif isinstance(obj, datetime.datetime):
+            return obj.__str__()
+
+        elif isinstance(obj, np.void):
+            return None
+        return json.JSONEncoder.default(self, obj)
 
 
 class ApiError(Exception):
