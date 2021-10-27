65763c3aad743046ed79ecc33a3d2b70409d767f
ChengFR <459763742@qq.com>
2021-10-27 19:50:21 +0800

    Feat: add interesting values when loading the entityset

--- vbridge/data_loader/data.py
+++ vbridge/data_loader/data.py
@@ -17,11 +17,14 @@ def create_entityset(dataset_id, entity_configs, relationships, table_dir, load_
         for table_name, info in entity_configs.items():
             table_df = pd.read_csv(os.path.join(table_dir, '{}.csv'.format(table_name)),
                                    date_parser=pd.to_datetime)
+            if dataset_id == 'mimic-demo':
+                table_df.columns = [col.upper() for col in table_df.columns]
             # Remove entries with missing identifiers
             index = info.get('index', table_df.columns[0])
             index_columns = info.get('identifiers', []) + [index]
             table_df = remove_nan_entries(table_df, index_columns, verbose=verbose)
 
+            # ALl identifiers are set as strings
             for col in index_columns:
                 table_df[col] = table_df[col].astype('str')
 
@@ -36,7 +39,19 @@ def create_entityset(dataset_id, entity_configs, relationships, table_dir, load_
             new_relationship = ft.Relationship(es[parent][primary_key], es[child][foreign_key])
             es = es.add_relationship(new_relationship)
 
+        # Add interesting values for categorical columns
+        for table_name, info in entity_configs.items():
+            if 'interesting_values' in info:
+                item_index = info['item_index']
+                interesting_values = info['interesting_values']
+                if interesting_values == 'ALL':
+                    interesting_values = es[table_name].df[item_index].unique()
+                elif isinstance(interesting_values, int):
+                    interesting_values = es[table_name].df[item_index] \
+                        .value_counts()[:interesting_values].index
+                es[table_name][item_index].interesting_values = interesting_values
+
         if save:
-            save_entityset(es, name=dataset_id)
+            save_entityset(es, dataset_id)
 
     return es
